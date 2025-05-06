3a38474bc1f0b625f5ae18d678ead8978bbff44e
Anthony Neubeck <tdnkvc@umsystem.edu>
2025-05-05 19:51:25 -0500

    deleted unused using statements

--- WeeklyToDo/TaskManager.cs
+++ WeeklyToDo/TaskManager.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+
 
 
 namespace ToDoApp
